ef726865b5af034af9aec14211c864088e9f5780
siamtbhuiyan <siamtbhuiyan@gmail.com>
2021-08-31 22:31:48 +0600

    Exercise 3.3

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ app.get("/api/persons", (request, response) => {
   return response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    return response.json(person);
+  } else {
+    return response.status(404).end();
+  }
+});
+
 app.get("/info", (request, response) => {
   const date = new Date();
   response.send(
